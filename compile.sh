80059cd583d36d4c8b72f11dae325c523c74704a
Thiago Guedes <thiagothiguedes@gmail.com>
2025-09-06 14:27:46 -0300

    Delete compile.sh

--- compile.sh
+++
@@ -1,7 +0,0 @@
-g++ facedetect_simple.cpp `pkg-config --cflags opencv4` `pkg-config --libs --static opencv4` -o facedetect_simple
-
-g++ facedetect_extra.cpp `pkg-config --cflags opencv4` `pkg-config --libs --static opencv4` -o facedetect_extra
-
-g++ laranja.cpp `pkg-config --cflags opencv4` `pkg-config --libs --static opencv4` -o laranja
-
-./laranja
